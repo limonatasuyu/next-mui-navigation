18de61d4b0ff72b5daa081a9939115f2246b506a
emre <emredilek6@gmail.com>
2022-10-07 04:36:39 +0300

    client side data fetching wroten

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,40 +3,30 @@ import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 import type { NextPage } from 'next';
 import { Box } from '@mui/material';
 
+import axios from 'axios'
+import useSWR from 'swr'
+
 const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'id', headerName: 'ID', width: 400 },
   { field: 'firstName', headerName: 'First name', width: 130 },
   { field: 'lastName', headerName: 'Last name', width: 130 },
-  { 
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params: GridValueGetterParams) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
 ];
 
-const rows = [
-  { id: 1, lastName: 'Dilek', firstName: 'Emre', age: 35 },
-  { id: 2, lastName: 'Yılmaz', firstName: 'Mehmet', age: 42 },
-  { id: 3, lastName: 'Başaran', firstName: 'Mustafa', age: 45 },
-  { id: 4, lastName: 'someOne', firstName: 'someOne', age: 16 },
-  { id: 5, lastName: 'BirininOğlu', firstName: 'Neslihan', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
 
-function DataTable() {
+type rawData = {
+  firstName: string
+  id: string
+  lastName: string 
+  picture: string
+  title : string
+}
+
+function DataTable(props: {data: rawData[]}) {
+  
+  const rows = props.data.map((i) => {
+    return {id : i.id, firstName: i.firstName, lastName: i.lastName}
+  })
+
   return (
     <Box sx={{ height: 400}}>
       <DataGrid
@@ -51,12 +41,19 @@ function DataTable() {
 }
 
 
+const fetcher = () => axios.get('https://dummyapi.io/data/v1/user?page=1&limit=20', {headers: {'app-id': '633f5f01d876772e0d9c5bfd'}}).then(res => res.data)
+
 const Home: NextPage = () => {
+
+  const {data, error} = useSWR('https://dummyapi.io/data/v1/user?page=1&limit=10', fetcher)
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
 
   return (
     <Box sx={{width: '100%'}}>
       <h1 style={{textAlign: 'center'}}>HomePage</h1>
-      <DataTable />
+      <DataTable data={data.data}/>
     </Box>
   )
 }
